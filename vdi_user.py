ca10221e862351dc0c3d9d19732a0d4745c11376
rcaseysymbio <rcasey@symbiosystems.com>
2015-03-13 18:17:15 -0700

    duhhhh

---
+++ vdi_user.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class vdi_user(models.Model)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	user_name = models.CharField(max_length=30)
+	uuid = models. # create unique hash based on first_name
+	user_appgroups = models. # create 
